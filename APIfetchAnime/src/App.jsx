b365a923a865f3a0f12bffbe566aace96c744f5e
Lex <74861308+GenghisChan1@users.noreply.github.com>
2025-03-07 23:45:23 +0800

    Update App.jsx

--- APIfetchAnime/src/App.jsx
+++ APIfetchAnime/src/App.jsx
@@ -3,66 +3,51 @@ import { useState, useEffect, useRef } from "react";
 import { fetchAnime } from "./assets/APImalfetch";
 
 export function Animecards(){
-  const [animeid, setAnimeID] = useState(Array.from({ length: 10 }, (_, i) => i + 1));
-  const [animelist, setAnimelist] = useState([]);
-  const observerRef = useRef(null);
-  const loadTriggerRef = useRef(null);
+  const [animelist, setAnimelist] = useState([Array.from({ length: 10 }, (_, i) => i + 1)]);
+  const [currentAnimeIDs, setCurrentAnimeIDs] = useState([]);
 
-  function loadanimeIds(){
-    setAnimeID((prevIds) =>{
-      let lastIds = prevIds[prevIds.length - 1];
-      let newIds = Array.from({ length: 10 }, (_, i) => i + lastIds + 1);
-      return [...prevIds, ...newIds];
+  function fetchdata(){
+    fetchAnime(currentAnimeIDs).then((anime) => {
+      setAnimelist((prevList) => [...prevList, anime]);
     });
   }
 
+  for (let i = 0; i < currentAnimeIDs.length; i++){
+    setTimeout(fetchdata, 333);
+  }
+
   useEffect(() => {
-    if (animeid.length === 0) return;
+    const timeout = setTimeout();
+    
+    setCurrentAnimeIDs((prevIDs) => {
+      return [Array.from({ length: 10 }, (_, i) => i + prevIDs[prevIDs.length - 1] + 1)];
+    });
 
-    let index = 0;
-
-    const interval = setInterval(() => {
-      if (index > animeid.length) {
-        clearInterval(interval);
-      }
-
-      fetchAnime(animeid[index]).then((anime) => {
+    function fetchdata(){
+      fetchAnime(currentAnimeIDs).then((anime) => {
         setAnimelist((prevList) => [...prevList, anime]);
       });
+    }
 
-      index++;
-    }, 3333)
-    
-    return () => clearInterval(interval);
-  }, [animeid]);  
+    for (let i = 0; i < currentAnimeIDs.length; i++){
+      timeout(fetchdata, 333);
+    }
 
-  useEffect(() => {
-    if (!loadTriggerRef.current) return;
+    return () => timeout.forEach(clearTimeout);
 
-    observerRef.current = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          loadanimeIds(); // Load more anime when reaching the end
-        }
-      },
-      { threshold: 1.0 }
-    );
+  }, []);
 
-    observerRef.current.observe(loadTriggerRef.current);
-
-    return () => observerRef.current?.disconnect();
-  }, [animelist]);
+  
 
   return (
     <>
-      {animelist.map((anime) => (
-        <div key={anime.title}>
+      {animelist.map((anime, index) => (
+        <div key={index}>
           <h2>{anime.title + " " + anime.id}</h2>
           <img src={anime.img} alt={anime.title} />
           <p>{anime.rate}</p>
         </div>
       ))}
-      <div ref={loadTriggerRef} style={{ height: "20px", background: "transparent" }}></div>
     </>
   );
 }
